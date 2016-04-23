da243cbde7281a362bb969934ec8f50031934d94
npcipav <npcipav@gmail.com>
2016-04-24 00:54:36 +0300

    connection test

--- server/js/connection.js
+++ server/js/connection.js
@@ -12,38 +12,40 @@
 (function() {
     // Set the connection to the mobile app.
     var host = "ws://" + window.location.hostname + "/";
-    var ws = new WebSocket(host);
+    //var ws = new WebSocket(host);
     var songWasStarted = false;
+    var ws = io();
 
     function onOpen() {
 	// After we connect, the server sends us data which will be handled in
 	// ``onmessage`` so we do nothing here.
     }
 
-    function onClose(event) {
-	if (!e.wasClean) {
-	    // Retry if connection failed.
-	    ws = new WebSocket(host);
-	    ws.onopen = onOpen;
-	    ws.onclose = onClose;
-	    if (!songWasStarted) {
-		ws.onmessage = onGotMessageOnStart;
-	    } else {
-		ws.onmessage = onGotMessageOnConnectionEstablished;
-	    }
-	}
-	songWasStarted = false;
-    }
+    // function onClose(event) {
+    // 	if (!e.wasClean) {
+    // 	    // Retry if connection failed.
+    // 	    ws = new WebSocket(host);
+    // 	    ws.onopen = onOpen;
+    // 	    ws.onclose = onClose;
+    // 	    if (!songWasStarted) {
+    // 		ws.onmessage = onGotMessageOnStart;
+    // 	    } else {
+    // 		ws.onmessage = onGotMessageOnConnectionEstablished;
+    // 	    }
+    // 	}
+    // 	songWasStarted = false;
+    // }
 
     function onGotMessageOnStart(event) {
 	// Receive song name and command sequence.
 	// TODO! Generate a movement string list from the supplied code.
 	var newEvent = new CustomEvent(
 	    'agSetupEvent',
-	    {detail: {song: event.song, commands: event.commands}}
+	    {detail: {song: event.song, bpm: event.bpm, commands: event.commands}}
 	);
 	document.dispatchEvent(newEvent);
-	ws.onmessage = onGotMessageOnConnectionEstablished;
+	//ws.onmessage = onGotMessageOnConnectionEstablished;
+	ws.on('message', onGotMessageOnConnectionEstablished);
 	songWasStarted = true;
     }
 
@@ -57,7 +59,8 @@
 	document.dispatchEvent(newEvent);
     }
 
-    ws.onopen = onOpen;
-    ws.onclose = onClose;
-    ws.onmessage = onGotMessageOnStart;
+    io.on('message', onGotMessageOnStart);
+    // ws.onopen = onOpen;
+    // ws.onclose = onClose;
+    // ws.onmessage = onGotMessageOnStart;
 })();
